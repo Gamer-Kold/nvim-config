fab26a206b03b9ec49a41953928bb5cff0623058
Talha Qamar <qamartalha@proton.me>
2024-03-21 07:59:59 +0500

    Added nvim-cmp

--- init.lua
+++ init.lua
@@ -77,8 +77,8 @@ require('lazy').setup({
 
   {
     "mg979/vim-visual-multi",
-    init = function ()
-      vim.g.VM_maps={
+    init = function()
+      vim.g.VM_maps = {
         ['Find Under'] = "\\\\<C-n>",
         ['Find Subword Under'] = "\\\\<C-n>",
       }
@@ -119,7 +119,81 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
+    config = function()
+      local cmp = require 'cmp'
+
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' }, -- For luasnip users.
+        }, {
+          { name = 'buffer' },
+        })
+      })
+
+      -- Set configuration for specific filetype.
+      cmp.setup.filetype('gitcommit', {
+        sources = cmp.config.sources({
+          { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        }, {
+          { name = 'buffer' },
+        })
+      })
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
+    end
+  },
 })
+
+
+
+
+
+-- Set up lspconfig.
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 
 vim.cmd.colorscheme "zenbones"
 
@@ -154,11 +228,14 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 -- The line beneath this is called `modeline`. See `:help modeline`
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lsp_conf = {
+  capabilities = capabilities
+}
 local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup {}
-lspconfig.tsserver.setup {}
-lspconfig.rust_analyzer.setup {}
-
+lspconfig.lua_ls.setup       (lsp_conf)
+lspconfig.tsserver.setup     (lsp_conf)
+lspconfig.rust_analyzer.setup(lsp_conf)
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
