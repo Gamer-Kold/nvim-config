6d0cffcf56f2f9015d024b22954c857f829773d9
Talha Qamar <qamartalha@proton.me>
2024-10-13 07:15:48 +0500

    Added Fennel to ensure installed TS Parsers

--- fnl/plugin/treesitter.fnl
+++ fnl/plugin/treesitter.fnl
@@ -1,6 +1,6 @@
 {1 :nvim-treesitter/nvim-treesitter
  :build ":TSUpdate"
- :opts {:ensure_installed [:bash :c :html :lua :markdown :vim :vimdoc]
+ :opts {:ensure_installed [:fennel :bash :c :html :lua :markdown :vim :vimdoc]
         :highlight {:enable true :additional_vim_regex_highlighting [:ruby]}
         :indent {:enable true :disable [:ruby]}}
  ; [[ Configure Treesitter ]] See `:help nvim-treesitter`
--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -2,4 +2,4 @@ local function _1_(_, opts)
   local ts = require("nvim-treesitter.configs")
   return ts.setup(opts)
 end
-return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", opts = {ensure_installed = {"bash", "c", "html", "lua", "markdown", "vim", "vimdoc"}, highlight = {enable = true, additional_vim_regex_highlighting = {"ruby"}}, indent = {enable = true, disable = {"ruby"}}}, config = _1_}
+return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", opts = {ensure_installed = {"fennel", "bash", "c", "html", "lua", "markdown", "vim", "vimdoc"}, highlight = {enable = true, additional_vim_regex_highlighting = {"ruby"}}, indent = {enable = true, disable = {"ruby"}}}, config = _1_}
